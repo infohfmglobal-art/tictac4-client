fa793e5c9011415ae135c904e0da446308d88e68
infohfmglobal-art <info.hfmglobal@gmail.com>
2025-10-30 16:45:55 +0400

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,38 +1,63 @@
 import { Board } from "./board.js";
 
+const LS_KEY = "t4_score_v1";
+
 export class Game {
-    constructor(size = 4) {
-        this.board = new Board(size);
-        this.currentPlayer = "X";
-        this.isGameOver = false;
-        this.winner = null;
+  constructor(size = 4) {
+    this.board = new Board(size);
+    this.currentPlayer = "X";
+    this.winner = null;
+    this.isGameOver = false;
+    this.score = this.loadScore();
+  }
+
+  loadScore() {
+    try {
+      const s = JSON.parse(localStorage.getItem(LS_KEY) || "{}");
+      return { X: s.X || 0, O: s.O || 0 };
+    } catch {
+      return { X: 0, O: 0 };
     }
+  }
+  saveScore() {
+    localStorage.setItem(LS_KEY, JSON.stringify(this.score));
+  }
 
-    makeMove(row, col) {
-        if (this.isGameOver) return false;
+  makeMove(row, col) {
+    if (this.isGameOver) return { moved:false };
+    const moved = this.board.makeMove(row, col, this.currentPlayer);
+    if (!moved) return { moved:false };
 
-        const moved = this.board.makeMove(row, col, this.currentPlayer);
-        if (!moved) return false;
-
-        const winner = this.board.checkWinner();
-        if (winner) {
-            this.isGameOver = true;
-            this.winner = winner;
-            return true;
-        }
-
-        this.togglePlayer();
-        return true;
+    const { winner, line } = this.board.checkWinner();
+    if (winner) {
+      this.isGameOver = true;
+      this.winner = winner;
+      this.score[winner] += 1;
+      this.saveScore();
+      return { moved:true, winLine: line };
     }
-
-    togglePlayer() {
-        this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+    if (this.board.isFull()) {
+      this.isGameOver = true;
+      return { moved:true, draw:true };
     }
+    this.togglePlayer();
+    return { moved:true };
+  }
 
-    reset() {
-        this.board = new Board(this.board.size);
-        this.currentPlayer = "X";
-        this.isGameOver = false;
-        this.winner = null;
-    }
+  togglePlayer() {
+    this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+  }
+
+  nextRound() {
+    this.board.reset();
+    this.isGameOver = false;
+    this.winner = null;
+    this.currentPlayer = "X";
+  }
+
+  resetAll() {
+    this.nextRound();
+    this.score = { X: 0, O: 0 };
+    this.saveScore();
+  }
 }
