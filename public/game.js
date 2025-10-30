64676ecab7d53d97db2d5db1729965c92783053a
infohfmglobal-art <info.hfmglobal@gmail.com>
2025-10-30 17:36:39 +0400

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,63 +1,24 @@
 import { Board } from "./board.js";
 
-const LS_KEY = "t4_score_v1";
-
 export class Game {
-  constructor(size = 4) {
-    this.board = new Board(size);
-    this.currentPlayer = "X";
+  constructor() {
+    this.board = new Board(3);
+    this.turn = "X";
     this.winner = null;
-    this.isGameOver = false;
-    this.score = this.loadScore();
   }
 
-  loadScore() {
-    try {
-      const s = JSON.parse(localStorage.getItem(LS_KEY) || "{}");
-      return { X: s.X || 0, O: s.O || 0 };
-    } catch {
-      return { X: 0, O: 0 };
-    }
-  }
-  saveScore() {
-    localStorage.setItem(LS_KEY, JSON.stringify(this.score));
+  move(r, c) {
+    if (this.winner) return false;
+    if (!this.board.makeMove(r, c, this.turn)) return false;
+
+    this.winner = this.board.checkWinner();
+    if (!this.winner) this.turn = this.turn === "X" ? "O" : "X";
+    return true;
   }
 
-  makeMove(row, col) {
-    if (this.isGameOver) return { moved:false };
-    const moved = this.board.makeMove(row, col, this.currentPlayer);
-    if (!moved) return { moved:false };
-
-    const { winner, line } = this.board.checkWinner();
-    if (winner) {
-      this.isGameOver = true;
-      this.winner = winner;
-      this.score[winner] += 1;
-      this.saveScore();
-      return { moved:true, winLine: line };
-    }
-    if (this.board.isFull()) {
-      this.isGameOver = true;
-      return { moved:true, draw:true };
-    }
-    this.togglePlayer();
-    return { moved:true };
-  }
-
-  togglePlayer() {
-    this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
-  }
-
-  nextRound() {
-    this.board.reset();
-    this.isGameOver = false;
+  reset() {
+    this.board = new Board(3);
+    this.turn = "X";
     this.winner = null;
-    this.currentPlayer = "X";
-  }
-
-  resetAll() {
-    this.nextRound();
-    this.score = { X: 0, O: 0 };
-    this.saveScore();
   }
 }
