737e1536e9e5c503acfc714dcd988cc1b81d343c
infohfmglobal-art <info.hfmglobal@gmail.com>
2025-10-31 20:38:50 +0400

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,5 +1,8 @@
-import { Board } from "./board.js";
+const clickSound = new Audio('/sound/click.mp3');
+const winSound = new Audio('/sound/win.mp3');
+const loseSound = new Audio('/sound/lose.mp3');
 
+import { Board } from "./board.js";
 export class Game {
   constructor() {
     this.board = new Board(3);
